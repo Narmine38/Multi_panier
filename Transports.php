2c5c06cb0ac9bcd440b73e601abd8c1987783abf
Tatoutaz <96110767+Narmine38@users.noreply.github.com>
2022-08-24 08:10:32 +0200

    Add files via upload

---
+++ Transports.php
@@ -0,0 +1,25 @@
+<?php
+
+$deliveryCompagny = [
+
+    "dhl" => [
+
+        "small_package" => 500,
+        "medium_package" => 10,
+        "big_package" => "Livraison Gratuite",
+        "delai" => "2 à 3 jours ouvrés"
+
+
+    ],
+
+    "ups" => [
+
+        "small_package" => 15,
+        "medium_package" => 25,
+        "big_package" => "Livraison Gratuite",
+        "delai" => "Livraison Demain"
+
+
+    ],
+
+];
--- cart.php
+++ cart.php
@@ -1,24 +1,35 @@
 <?php
 session_start();
 require 'my-functions.php';
+require 'Transports.php';
+var_dump($_SESSION['cart']);
+foreach ($_SESSION['cart'] as $key => $value) {
+    $total = $_SESSION['cart'][$key]['price'] * $_SESSION['cart'][$key]['quantity'];
+    $priceTotal += $total;
+}
 ?>
-<pre>
     <a href="<?php emptyCart() ?>">session_destroy</a>
-    <?php var_dump($_SESSION['cart']); ?>
-
-</pre>
-
 
 <?php
 foreach ($_SESSION['cart'] as $key => $value) { ?>
     <h2>Articles : <?php echo $_SESSION['cart'][$key]['name'] ?></h2>
     <h2>Prix a l'unité : <?php echo formatPrice($_SESSION['cart'][$key]['price']) ?></h2>
     <h2>Quantity : <?php echo $_SESSION['cart'][$key]['quantity'] ?></h2>
-    <h2>prix
-        Total: <?php echo formatPrice(total($_SESSION['cart'][$key]['price'], $_SESSION['cart'][$key]['quantity'])) ?></h2>
 <?php } ?>
 
-<!--<h6> Prix finale : --><?php //echo  ?><!--</h6>-->
+<h6> Prix Tota : <?php echo formatPrice($priceTotal)  ?></h6>
+<h6> Prix HTT : <?php echo formatPrice(priceExcludingVAT($priceTotal))  ?></h6>
+<h6> tva : <?php echo formatPrice($priceTotal - priceExcludingVAT($priceTotal) ) ?></h6>
+
+<form method="post" >
+    <select name="delivery" class="me-5 rounded-pill" style="width: 60%">
+        <option value="">Sélectionnez un transporteur</option>
+        <?php foreach ($deliveryCompagny as $Key => $delivery) { ?>
+            <option value="<?= $Key ?>"> <?= $Key ?></option>
+        <?php } ?>
+    </select>
+    <input style="width: 200px" class="btn btn-primary" type="submit" value="Valider">
+</form>
 
 
 
